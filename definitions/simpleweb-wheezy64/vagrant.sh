306b92914e87dbc5ce1aab2104e091f02e2e3552
Peter Rhoades <pete@simpleweb.co.uk>
2014-01-28 11:41:39 +0000

    Getting rid of annoying stdinL is not a tty error

--- definitions/simpleweb-wheezy64/vagrant.sh
+++ definitions/simpleweb-wheezy64/vagrant.sh
@@ -10,3 +10,14 @@ cd /home/vagrant/.ssh
 wget --no-check-certificate 'https://raw.github.com/mitchellh/vagrant/master/keys/vagrant.pub' -O authorized_keys
 chmod 600 /home/vagrant/.ssh/authorized_keys
 chown -R vagrant /home/vagrant/.ssh
+
+# Prevent future `stdin: is not a tty` errors
+if [ "`tail -1 /root/.profile`" = "mesg n" ]; then
+  echo 'Patching basebox to prevent future `stdin: is not a tty` errors...'
+  sed -i '$d' /root/.profile
+  cat << 'EOH' >> /root/.profile
+  if `tty -s`; then
+    mesg n
+  fi
+EOH
+fi
