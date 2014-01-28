c0b6e471e3b4ec79026cdad82d2a04905d005573
Peter Rhoades <pete@simpleweb.co.uk>
2014-01-28 15:04:33 +0000

    Wrong codename was used

--- definitions/simpleweb-wheezy64/puppet.sh
+++ definitions/simpleweb-wheezy64/puppet.sh
@@ -1,6 +1,6 @@
 # Prepare puppetlabs repo
-wget http://apt.puppetlabs.com/puppetlabs-release-precise.deb
-dpkg -i puppetlabs-release-precise.deb
+wget http://apt.puppetlabs.com/puppetlabs-release-wheezy.deb
+dpkg -i puppetlabs-release-wheezy.deb
 apt-get update
 
 # Install puppet/facter
